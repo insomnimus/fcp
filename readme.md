a25510e49eb3668cddb47343c4639c1d50b4ca78
Taylan Gökkaya <insomnimus.uni@gmail.com>
2021-06-18 00:50:57 +0300

    '[metadata]: added crate metadata and documentation'

--- readme.md
+++ readme.md
@@ -2,18 +2,74 @@
 
 A tiny protocol to transmit fan control messages.
 
-Written to work on embedded systems.
+Written for systems where resources are not taken as granted.
+
+For example there's absolutely no stdlib dependency (no_std).
+This crate also doesn't assume any sort of heap allocator; all operations use the stack and very sparingly .
+
+FCP does not use floating point numbers.
+The fine tuning is done by assuming values are, for example in millivolts instead of volts.
+The reasoning behind this is, not all microcontrollers come with a floating point unit.
+
+# Stability and Versioning
+
+Any major release is not backwards compatible.
+
+Any new minor release, up to 1.0 may or may not break backwards compatibility.
+Any new minor release after 1.0 will not break backwards compatibility unless the release is security related or critical.
+
+Any new patch release is always backwards compatible.
 
 # Requests
+
+### SET
+
+SET requests are made to set a variable such as the operating speed percentage.
+
+The syntax for SET requests is:
+
+`SET <CHAR>[VALUE]`
+
+where
+
+-	CHAR: a one byte character, denoting the target to set. Letters must be lowercase.
+-	VALUE: any ASCII encoded string without any space characters.
 
 ```
 SET %<u8> # set speed percentage
 SET v<u16> # set voltage in millivolts
 SET a # let the device decide
+```
 
+### ADJ
+
+ADJ (adjust) requests are made to tune the device parameters relative to the current setting.
+The syntax for ADJ requests is:
+
+`ADJ <CHAR><VALUE>`
+
+where
+
+-	CHAR: a one byte character, denoting the target to set. Letters must be lowercase.
+-	VALUE: A numeric value, consisting of digits and (as a prefix and only once) the plus (`+`) or the minus (`-`) signs.
+
+```
 ADJ v<i16> # adjust the voltage in millivolts
 ADJ %<i8> # adjust the speed percentage
+```
 
+### GET
+
+GET requests are made to get the various readings from the operating device such as the temperature.
+The syntax for GET requests is:
+
+`GET <PARAMETER>`
+
+where
+
+PARAMETER: the reading or setting, ASCII string with no space characters.
+
+```
 GET volt # get current voltage in millivolts
 get cfg # get if manual or auto
 GET temp # get temperature in celcius
@@ -29,15 +85,11 @@ ASCII.
 
 Any lossless connection like TCP or Serial Over Bluetooth.
 
-# Messages
-
-`<RequestType> <value>`
-
-Messages are terminated by a `\0` (null byte).
+Messages are terminated by a semicolon (`;`).
 
 # Responses
 
-There're two response types:
+There are two response types:
 
 -	Ok: status code 0.
 -	Err: status code 1.
--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,48 @@
+//! This crate provides types and methods for parsing/ creating [FCP](https://github.com/insomnimus/fcp) requests.
+//!
+//! # Examples
+//!
+//! ```
+//! use fcp::{Request, AdjRequest, GetRequest, SetRequest};
+//!
+//! // Imagine we have an incoming fcp request
+//! // over some connection. We can parse the request like this.
+//! let req= b"GET all"; // the bytes we received over the connection
+//! let req = Request::parse(req).unwrap();
+//! assert_eq!(Request::Get(GetRequest::All), req);
+//!
+//! // We can also use the string extension method:
+//! let req: Request = "SET v500".parse().unwrap();
+//! assert_eq!(Request::Set(SetRequest::Voltage(500)), req);
+//!
+//! // On the client side, we may form requests like this:
+//! let req = Request::Adj(AdjRequest::Voltage(-25));
+//! let req_string= format!("{};", &req); // fcp requests are ';' terminated
+//! assert_eq!("ADJ v-25;", req_string.as_str());
+
 #![cfg_attr(not(test), no_std)]
 
 #[cfg(test)]
 mod tests;
 
-use core::{fmt, str};
+use core::{
+    fmt,
+    str::{self, FromStr},
+};
 use Error::*;
 
 type Result<T> = core::result::Result<T, Error>;
 
-#[derive(Debug, PartialEq)]
+/// Crate specific (not FCP bound) errors that may be returned trying to parse Requests.
+#[derive(Debug, PartialEq, Eq)]
 pub enum Error {
+    /// The request method word is unknown. For example "FETCH x" (FETCH is invalid).
     UnknownRequestType,
+    /// The request has an invalid value. For example "GET shoesize".
     InvalidValue,
+    /// The request consists of just the method. For example "SET".
     MissingValue,
+    /// The request is empty. For example "".
     Empty,
 }
 
@@ -31,14 +61,103 @@ impl fmt::Display for Error {
     }
 }
 
-#[derive(Debug, PartialEq)]
+/// Types of FCP requests.
+#[derive(Debug, PartialEq, Eq)]
 pub enum Request {
+    /// The `GET`request.
     Get(GetRequest),
+    /// The `SET` request.
     Set(SetRequest),
+    /// The `ADJ` (adjust) request.
     Adj(AdjRequest),
 }
 
-#[derive(Debug, PartialEq)]
+impl fmt::Display for Request {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::Get(r) => write!(f, "GET {}", r.val_str()),
+            Self::Set(r) => {
+                use SetRequest::*;
+                match r {
+                    Voltage(v) => write!(f, "SET v{}", v),
+                    Percentage(v) => write!(f, "SET %{}", v),
+                    Auto => write!(f, "SET a"),
+                }
+            }
+            Self::Adj(r) => {
+                use AdjRequest::*;
+                match r {
+                    Voltage(v) => write!(f, "ADJ v{}", v),
+                    Percentage(v) => write!(f, "ADJ %{}", v),
+                }
+            }
+        }
+    }
+}
+
+impl Request {
+    /// Tries to parse a slice of bytes into a Request.
+    ///
+    /// # Errors
+    /// `parse` will error if the given byte slice doesn't contain valid ASCII or
+    /// the byte slice is not a valid FCP request.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use fcp::{Request, SetRequest};
+    /// let req = b"SET %25";
+    /// assert_eq!(Ok(Request::Set(SetRequest::Percentage(25))), Request::parse(req));
+    /// ```
+    pub fn parse(s: &[u8]) -> Result<Self> {
+        let mut split = s.splitn(2, |b| *b == b' ');
+        let method = match split.next() {
+            None => return Err(Empty),
+            Some(x) => x,
+        };
+        if let Some(val) = split.next() {
+            match method {
+                b"GET" => GetRequest::parse(val).map(Self::Get),
+                b"SET" => SetRequest::parse(val).map(Self::Set),
+                b"ADJ" => AdjRequest::parse(val).map(Self::Adj),
+                _ => Err(UnknownRequestType),
+            }
+        } else {
+            match method {
+                b"GET" | b"SET" | b"ADJ" => Err(MissingValue),
+                b"" => Err(Empty),
+                _ => Err(UnknownRequestType),
+            }
+        }
+    }
+
+    /// Returns the method name, as a static &str.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use fcp::{Request, GetRequest};
+    /// let req = Request::Get(GetRequest::Config);
+    /// assert_eq!("GET", req.method());
+    /// ```
+    pub fn method(&self) -> &'static str {
+        match self {
+            Self::Get(_) => "GET",
+            Self::Set(_) => "SET",
+            Self::Adj(_) => "ADJ",
+        }
+    }
+}
+
+impl FromStr for Request {
+    type Err = Error;
+    fn from_str(s: &str) -> Result<Self> {
+        Self::parse(s.as_bytes())
+    }
+}
+
+/// Types of `GET` FCP requests.
+#[derive(Debug, PartialEq, Eq)]
 pub enum GetRequest {
     All,
     Config,
@@ -71,7 +190,8 @@ impl GetRequest {
     }
 }
 
-#[derive(Debug, PartialEq)]
+/// Types of `SET` FCP requests.
+#[derive(Debug, PartialEq, Eq)]
 pub enum SetRequest {
     Auto,
     Voltage(u16),
@@ -107,7 +227,8 @@ impl SetRequest {
     }
 }
 
-#[derive(Debug, PartialEq)]
+/// Types of `ADJ` (adjust) FCP requests.
+#[derive(Debug, PartialEq, Eq)]
 pub enum AdjRequest {
     Voltage(i16),
     Percentage(i8),
@@ -136,61 +257,6 @@ impl AdjRequest {
                     }
                 }
                 _ => Err(InvalidValue),
-            }
-        }
-    }
-}
-
-impl Request {
-    pub fn parse(s: &[u8]) -> Result<Self> {
-        let mut split = s.splitn(2, |b| *b == b' ');
-        let method = match split.next() {
-            None => return Err(Empty),
-            Some(x) => x,
-        };
-        if let Some(val) = split.next() {
-            match method {
-                b"GET" => GetRequest::parse(val).map(Self::Get),
-                b"SET" => SetRequest::parse(val).map(Self::Set),
-                b"ADJ" => AdjRequest::parse(val).map(Self::Adj),
-                _ => Err(UnknownRequestType),
-            }
-        } else {
-            match method {
-                b"GET" | b"SET" | b"ADJ" => Err(MissingValue),
-                b"" => Err(Empty),
-                _ => Err(UnknownRequestType),
-            }
-        }
-    }
-
-    pub fn method(&self) -> &'static str {
-        match self {
-            Self::Get(_) => "GET",
-            Self::Set(_) => "SET",
-            Self::Adj(_) => "ADJ",
-        }
-    }
-}
-
-impl fmt::Display for Request {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Self::Get(r) => write!(f, "GET {}", r.val_str()),
-            Self::Set(r) => {
-                use SetRequest::*;
-                match r {
-                    Voltage(v) => write!(f, "SET v{}", v),
-                    Percentage(v) => write!(f, "SET %{}", v),
-                    Auto => write!(f, "SET a"),
-                }
-            }
-            Self::Adj(r) => {
-                use AdjRequest::*;
-                match r {
-                    Voltage(v) => write!(f, "ADJ v{}", v),
-                    Percentage(v) => write!(f, "ADJ %{}", v),
-                }
             }
         }
     }
